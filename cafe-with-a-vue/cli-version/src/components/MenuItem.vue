30ef3468ab4de8873a242054b803a8c9b2e74073
Ben Hong <bencodezen@gmail.com>
2019-10-30 08:43:18 -0400

    feature: Add sale price generation

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,7 +1,28 @@
 <script>
 export default {
   name: 'MenuItem',
-  props: ['addToShoppingCart', 'image', 'inStock', 'name', 'price', 'quantity']
+  props: ['addToShoppingCart', 'image', 'inStock', 'name', 'price', 'quantity'],
+  data() {
+    return {
+      onSale: false
+    }
+  },
+  computed: {
+    generatedPrice() {
+      if (this.onSale) {
+        return (this.price * 0.9).toFixed(2)
+      } else {
+        return this.price
+      }
+    }
+  },
+  beforeMount() {
+    const today = new Date().getDate()
+
+    if (today % 2 === 0) {
+      this.onSale = true
+    }
+  }
 }
 </script>
 
@@ -10,9 +31,9 @@ export default {
     <img class="menu-item__image" :src="image.source" :alt="image.alt" />
     <div>
       <h3>{{ name }}</h3>
+      <p>Price: {{ generatedPrice }}</p>
       <p v-if="inStock">In Stock</p>
       <p v-else>Out of Stock</p>
-      <p>Price: {{ price }}</p>
       <div>
         <label for="add-item-quantity">Quantity: {{ quantity }}</label>
         <input v-model.number="quantity" id="add-item-quantity" type="number" />
