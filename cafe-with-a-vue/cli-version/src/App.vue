e7590365c9dc54917bb2c8daa8dae50864dbe3d1
Ben Hong <bencodezen@gmail.com>
2019-10-30 08:26:39 -0400

    feature: Add Vue Router

--- cafe-with-a-vue/cli-version/src/App.vue
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -1,101 +1,12 @@
 <template>
   <div id="app">
-    <h1>{{ restaurantName }}</h1>
-    <p>
-      Welcome to {{ restaurantName }}! We are known for our freshly baked bread
-      and french pastries! Give you morning a warm start or treat yourself in
-      the middle of the day. Our butter is imported from local farmers in
-      France. Once you take your first bite, you will see why everyone can't get
-      enough!
-    </p>
-
-    <h2>Menu</h2>
-    <MenuItem
-      v-for="item in simpleMenu"
-      :addToShoppingCart="addToShoppingCart"
-      :name="item.name"
-      :image="item.image"
-      :quantity="item.quantity"
-      :inStock="item.inStock"
-      :key="item.name"
-    />
-
-    <h2>Shopping Cart: {{ shoppingCart }} items</h2>
-
-    <h2>Contact Us</h2>
-    <p>Address: {{ address }}</p>
-    <p>Phone: {{ phone }}</p>
-    <p>Email: {{ email }}</p>
-    <p>Hours:</p>
-    <ul>
-      <li>M-F: 06:00 to 16:00</li>
-      <li>Saturday: 07:00 to 14:00</li>
-      <li>Sunday: 07:00 to 12:00</li>
-    </ul>
-    <footer>{{ copyright }}</footer>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
-
-<script>
-import MenuItem from './components/MenuItem'
-
-export default {
-  name: 'App',
-  components: {
-    MenuItem
-  },
-  data() {
-    return {
-      address: '9274 Beurre Ave, Paris, France',
-      email: 'hello@cafewithavue.bakery',
-      phone: '+08 88 88 88 88 88',
-      restaurantName: 'Cafe with A Vue',
-      shoppingCart: 0,
-      simpleMenu: [
-        {
-          name: 'Crossiant',
-          image: {
-            source: '/images/crossiant.jpg',
-            alt: 'A crossiant'
-          },
-          inStock: true,
-          quantity: 1
-        },
-        {
-          name: 'French Baguette',
-          image: {
-            source: '/images/french-baguette.jpeg',
-            alt: 'Four French Baguettes'
-          },
-          inStock: true,
-          quantity: 1
-        },
-        {
-          name: 'Éclair',
-          image: {
-            source: '/images/eclair.jpg',
-            alt: 'Chocolate Éclair'
-          },
-          inStock: false,
-          quantity: 1
-        }
-      ]
-    }
-  },
-  computed: {
-    copyright() {
-      const currentYear = new Date().getFullYear()
-
-      return `Copyright ${this.restaurantName} ${currentYear}`
-    }
-  },
-  methods: {
-    addToShoppingCart(amount) {
-      this.shoppingCart += amount
-    }
-  }
-}
-</script>
 
 <style lang="scss">
 #app {
@@ -104,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ cafe-with-a-vue/cli-version/src/views/Home.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <h1>{{ restaurantName }}</h1>
+    <p>
+      Welcome to {{ restaurantName }}! We are known for our freshly baked bread
+      and french pastries! Give you morning a warm start or treat yourself in
+      the middle of the day. Our butter is imported from local farmers in
+      France. Once you take your first bite, you will see why everyone can't get
+      enough!
+    </p>
+
+    <h2>Menu</h2>
+    <MenuItem
+      v-for="item in simpleMenu"
+      :addToShoppingCart="addToShoppingCart"
+      :name="item.name"
+      :image="item.image"
+      :quantity="item.quantity"
+      :inStock="item.inStock"
+      :key="item.name"
+    />
+
+    <h2>Shopping Cart: {{ shoppingCart }} items</h2>
+
+    <h2>Contact Us</h2>
+    <p>Address: {{ address }}</p>
+    <p>Phone: {{ phone }}</p>
+    <p>Email: {{ email }}</p>
+    <p>Hours:</p>
+    <ul>
+      <li>M-F: 06:00 to 16:00</li>
+      <li>Saturday: 07:00 to 14:00</li>
+      <li>Sunday: 07:00 to 12:00</li>
+    </ul>
+    <footer>{{ copyright }}</footer>
+  </div>
+</template>
+
+<script>
+import MenuItem from '../components/MenuItem'
+
+export default {
+  name: 'Home',
+  components: {
+    MenuItem
+  },
+  data() {
+    return {
+      address: '9274 Beurre Ave, Paris, France',
+      email: 'hello@cafewithavue.bakery',
+      phone: '+08 88 88 88 88 88',
+      restaurantName: 'Cafe with A Vue',
+      shoppingCart: 0,
+      simpleMenu: [
+        {
+          name: 'Crossiant',
+          image: {
+            source: '/images/crossiant.jpg',
+            alt: 'A crossiant'
+          },
+          inStock: true,
+          quantity: 1
+        },
+        {
+          name: 'French Baguette',
+          image: {
+            source: '/images/french-baguette.jpeg',
+            alt: 'Four French Baguettes'
+          },
+          inStock: true,
+          quantity: 1
+        },
+        {
+          name: 'Éclair',
+          image: {
+            source: '/images/eclair.jpg',
+            alt: 'Chocolate Éclair'
+          },
+          inStock: false,
+          quantity: 1
+        }
+      ]
+    }
+  },
+  computed: {
+    copyright() {
+      const currentYear = new Date().getFullYear()
+
+      return `Copyright ${this.restaurantName} ${currentYear}`
+    }
+  },
+  methods: {
+    addToShoppingCart(amount) {
+      this.shoppingCart += amount
+    }
+  }
+}
+</script>
