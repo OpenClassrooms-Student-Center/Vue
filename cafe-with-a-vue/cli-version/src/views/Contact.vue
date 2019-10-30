5212961f26786b269fd4c456deca7aee5b657bda
Ben Hong <bencodezen@gmail.com>
2019-10-30 08:32:49 -0400

    refactor: Migrate data into Contact page

---
+++ cafe-with-a-vue/cli-version/src/views/Contact.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'Contact',
+  data() {
+    return {
+      address: '9274 Beurre Ave, Paris, France',
+      email: 'hello@cafewithavue.bakery',
+      phone: '+08 88 88 88 88 88'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h2>Contact Us</h2>
+    <p>Address: {{ address }}</p>
+    <p>Phone: {{ phone }}</p>
+    <p>Email: {{ email }}</p>
+    <p>Hours:</p>
+    <ul>
+      <li>M-F: 06:00 to 16:00</li>
+      <li>Saturday: 07:00 to 14:00</li>
+      <li>Sunday: 07:00 to 12:00</li>
+    </ul>
+  </div>
+</template>
+
+<style></style>
--- cafe-with-a-vue/cli-version/src/views/Home.vue
+++ cafe-with-a-vue/cli-version/src/views/Home.vue
@@ -22,16 +22,6 @@
 
     <h2>Shopping Cart: {{ shoppingCart }} items</h2>
 
-    <h2>Contact Us</h2>
-    <p>Address: {{ address }}</p>
-    <p>Phone: {{ phone }}</p>
-    <p>Email: {{ email }}</p>
-    <p>Hours:</p>
-    <ul>
-      <li>M-F: 06:00 to 16:00</li>
-      <li>Saturday: 07:00 to 14:00</li>
-      <li>Sunday: 07:00 to 12:00</li>
-    </ul>
     <footer>{{ copyright }}</footer>
   </div>
 </template>
@@ -46,9 +36,6 @@ export default {
   },
   data() {
     return {
-      address: '9274 Beurre Ave, Paris, France',
-      email: 'hello@cafewithavue.bakery',
-      phone: '+08 88 88 88 88 88',
       restaurantName: 'Cafe with A Vue',
       shoppingCart: 0,
       simpleMenu: [
