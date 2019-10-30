4b8c40fb9d7d10ea457fa6b87ee0c30cae20d0d2
Ben Hong <bencodezen@gmail.com>
2019-10-30 09:15:52 -0400

    refactor: Move data to Vuex state

--- cafe-with-a-vue/cli-version/src/store/index.js
+++ cafe-with-a-vue/cli-version/src/store/index.js
@@ -5,11 +5,42 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    restaurantName: 'Cafe with A Vue',
+    shoppingCart: 0,
+    simpleMenu: [
+      {
+        name: 'Crossiant',
+        image: {
+          source: '/images/crossiant.jpg',
+          alt: 'A crossiant'
+        },
+        inStock: true,
+        quantity: 1,
+        price: 2.99
+      },
+      {
+        name: 'French Baguette',
+        image: {
+          source: '/images/french-baguette.jpeg',
+          alt: 'Four French Baguettes'
+        },
+        inStock: true,
+        quantity: 1,
+        price: 3.99
+      },
+      {
+        name: 'Éclair',
+        image: {
+          source: '/images/eclair.jpg',
+          alt: 'Chocolate Éclair'
+        },
+        inStock: false,
+        quantity: 1,
+        price: 4.99
+      }
+    ]
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  mutations: {},
+  actions: {},
+  modules: {}
 })
--- cafe-with-a-vue/cli-version/src/views/Home.vue
+++ cafe-with-a-vue/cli-version/src/views/Home.vue
@@ -33,56 +33,24 @@
 
 <script>
 import MenuItem from '../components/MenuItem'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
   components: {
     MenuItem
   },
-  data() {
-    return {
-      restaurantName: 'Cafe with A Vue',
-      shoppingCart: 0,
-      simpleMenu: [
-        {
-          name: 'Crossiant',
-          image: {
-            source: '/images/crossiant.jpg',
-            alt: 'A crossiant'
-          },
-          inStock: true,
-          quantity: 1,
-          price: 2.99
-        },
-        {
-          name: 'French Baguette',
-          image: {
-            source: '/images/french-baguette.jpeg',
-            alt: 'Four French Baguettes'
-          },
-          inStock: true,
-          quantity: 1,
-          price: 3.99
-        },
-        {
-          name: 'Éclair',
-          image: {
-            source: '/images/eclair.jpg',
-            alt: 'Chocolate Éclair'
-          },
-          inStock: false,
-          quantity: 1,
-          price: 4.99
-        }
-      ]
-    }
-  },
   computed: {
     copyright() {
       const currentYear = new Date().getFullYear()
 
       return `Copyright ${this.restaurantName} ${currentYear}`
-    }
+    },
+    ...mapState({
+      restaurantName: 'restaurantName',
+      shoppingCart: 'shoppingCart',
+      simpleMenu: 'simpleMenu'
+    })
   },
   methods: {
     addToShoppingCart(amount) {
