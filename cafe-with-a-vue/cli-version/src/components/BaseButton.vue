95d6dd90c82d18e5518da816e8059755f5d46ca3
Ben Hong <bencodezen@gmail.com>
2019-10-30 10:23:32 -0400

    fix: Add v-on listener so BaseButton can react to events

--- cafe-with-a-vue/cli-version/src/components/BaseButton.vue
+++ cafe-with-a-vue/cli-version/src/components/BaseButton.vue
@@ -6,7 +6,11 @@ export default {
 </script>
 
 <template>
-  <button>
+  <!-- This v-on="$listeners" is a way
+  to assign events to an element in a component.
+  Without it, Vue does not know where to listen
+  for the events. -->
+  <button v-on="$listeners">
     {{ text }}
   </button>
 </template>
