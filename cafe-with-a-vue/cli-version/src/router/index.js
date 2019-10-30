38e0f782ec2e5632451dd2ca30f08c501745b621
Ben Hong <bencodezen@gmail.com>
2019-10-30 08:32:26 -0400

    feature: Add contact route

--- cafe-with-a-vue/cli-version/src/router/index.js
+++ cafe-with-a-vue/cli-version/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Contact from '../views/Contact.vue'
 
 Vue.use(VueRouter)
 
@@ -16,7 +17,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
   }
 ]
 
