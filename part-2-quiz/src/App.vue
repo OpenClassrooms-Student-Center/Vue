e0359653d26bd6dbc4ee77839e99e173056fcbf8
Ben Hong <bencodezen@gmail.com>
2019-12-24 08:51:54 -0500

    feature: Add Question #8 required code

--- part-2-quiz/src/App.vue
+++ part-2-quiz/src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <a href="/about">About</a>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
