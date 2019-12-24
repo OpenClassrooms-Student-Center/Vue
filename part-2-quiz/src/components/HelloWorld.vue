86a88ed26060f10108c668c58319ce814e86a700
Ben Hong <bencodezen@gmail.com>
2019-12-24 08:40:46 -0500

    feature: Add Question #2 required code

--- part-2-quiz/src/components/HelloWorld.vue
+++ part-2-quiz/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ title }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -89,8 +89,8 @@
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data: {
+    title: 'OpenClassrooms - Part 2 Quiz'
   }
 }
 </script>
