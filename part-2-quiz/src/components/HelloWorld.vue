d26f7a7211ec70365d83e2447199f3f02af8e10e
Ben Hong <bencodezen@gmail.com>
2019-12-24 08:48:29 -0500

    feature: Add Question #3 required code

--- part-2-quiz/src/components/HelloWorld.vue
+++ part-2-quiz/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ title }}</h1>
+    <Counter />
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -91,6 +92,9 @@ export default {
   name: 'HelloWorld',
   data: {
     title: 'OpenClassrooms - Part 2 Quiz'
+  },
+  components: {
+    Counter
   }
 }
 </script>
