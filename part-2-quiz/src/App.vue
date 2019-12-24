e9e9273e71c7b2c6a552cc6e2dd602a4ffcb4e80
Ben Hong <bencodezen@gmail.com>
2019-12-24 08:56:42 -0500

    feature: Add Login link for Question #4

--- part-2-quiz/src/App.vue
+++ part-2-quiz/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <a href="/about">About</a>
+      <router-link to="/">Home</router-link> | <a href="/about">About</a> |
+      <router-link to="/login">Login</router-link>
     </div>
     <router-view />
   </div>
