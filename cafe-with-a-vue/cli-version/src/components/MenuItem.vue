00c01577763f8b031e9af5e02bc0350503aa1c9f
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-04-23 11:19:26 +0200

    p2c3 solution trad change index.html

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,26 +1,26 @@
 <script>
 export default {
-  name: 'MenuItem',
-  props: ['addToShoppingCart', 'image', 'inStock', 'name', 'quantity']
+	name: "MenuItem",
+	props: ["addToShoppingCart", "image", "inStock", "name", "quantity"]
 }
 </script>
 
 <template>
-  <div class="menu-item">
-    <img class="menu-item__image" :src="image.source" :alt="image.alt" />
-    <div>
-      <h3>{{ name }}</h3>
-      <p v-if="inStock">In Stock</p>
-      <p v-else>Out of Stock</p>
-      <div>
-        <label for="add-item-quantity">Quantity: {{ quantity }}</label>
-        <input v-model.number="quantity" id="add-item-quantity" type="number" />
-        <button @click="addToShoppingCart(quantity)">
-          Add to Shopping Cart
-        </button>
-      </div>
-    </div>
-  </div>
+	<div class="menu-item">
+		<img class="menu-item__image" :src="image.source" :alt="image.alt" />
+		<div>
+			<h3>{{ name }}</h3>
+			<p v-if="inStock">En stock</p>
+			<p v-else>En rupture de stock</p>
+			<div>
+				<label for="add-item-quantity">Quantité : {{ quantity }}</label>
+				<input v-model.number="quantity" id="add-item-quantity" type="number" />
+				<button @click="addToShoppingCart(quantity)">
+					Ajouter au panier d'achat
+				</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style></style>
